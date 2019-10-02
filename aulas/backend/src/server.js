e8990dd68b957096012edbefcbbc64b7945f8ce6
MrWillian <willian.dev10@gmail.com>
2019-10-02 10:34:45 -0300

    first commit

---
+++ aulas/backend/src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-58x2s.mongodb.net/aircnc?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// req.query = Acessar query params (parametros passados pela url (com ?) - para filtros)
+// req.params = Acessar route params (parametros passados pela url (com /) - para edição e delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+// req.headers = Serve para definir o contexto da requisição (contexto de autenticação, idioma do usuário)
+
+app.use(express.json()); //Necessário para que o express entenda o JSON
+app.use(routes); // Precisa vir depois da linha do express.json()
+
+app.listen('3333');
