4c87094a883f7ac07ec3110a96226a8408fdd7b6
MrWillian <willian.dev10@gmail.com>
2019-10-07 08:18:08 -0300

    Add loading effect with ShimmerPlaceHolder for itens in the  list of spots

--- aulas/aircnc/src/components/SpotList.js
+++ aulas/aircnc/src/components/SpotList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { withNavigation } from 'react-navigation';
 import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
+import ShimmerPlaceHolder from 'react-native-shimmer-placeholder';
 
 import api from '../services/api';
 
@@ -8,10 +9,12 @@ function SpotList({ tech, navigation }) {
   const [spots, setSpots] = useState([]);
 
   useEffect(() => {
+    spots.visible = false;
     async function loadSpots() {
       const response = await api.get('/spots', {
         params: { tech }
       });
+      response.data.visible = true;
       setSpots(response.data);
     }
     loadSpots();
@@ -33,12 +36,23 @@ function SpotList({ tech, navigation }) {
         showsHorizontalScrollIndicator={false}
         renderItem={({ item }) => (
           <View style={styles.listItem}>
-            <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url}} />
-            <Text style={styles.company}>{item.company}</Text>
-            <Text style={styles.price}>{item.price ? `R$${item.price}` : 'GRATUITO'}</Text>
+
+            <ShimmerPlaceHolder style={{ width: 200, height: 110}} autoRun={true} visible={true}>
+              <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url}} />
+            </ShimmerPlaceHolder>
+
+            <ShimmerPlaceHolder style={{ width: '100%', height: 15}} autoRun={true} visible={true}>
+              <Text style={styles.company}>{item.company}</Text>
+            </ShimmerPlaceHolder>
+
+            <ShimmerPlaceHolder style={{ width: '100%', height: 10}} autoRun={true} visible={true}>
+              <Text style={styles.price}>{item.price ? `R$${item.price}` : 'GRATUITO'}</Text>
+            </ShimmerPlaceHolder>
+
             <TouchableOpacity onPress={() => handleNavigation(item._id)} style={styles.button}>
               <Text style={styles.buttonText}>Solicitar reserva</Text>
             </TouchableOpacity>
+            
           </View>
         )}
         />
